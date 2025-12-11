78b40848f8870e1a17d58cc2dcbf9e375186ed0f
lyns0 <disse.sl@proton.me>
2025-12-11 18:59:55 +0700

    feat: implement some of Player

--- firefly-core/src/player/mod.rs
+++ firefly-core/src/player/mod.rs
@@ -1,6 +1,49 @@
+use anyhow::Result;
+use mpris_server::{PlaybackRate, PlaybackStatus, Volume};
+use rodio::{OutputStream, OutputStreamBuilder, Sink};
+
 #[cfg(test)]
-pub mod test;
+pub mod tests;
 
 pub mod mpris;
 
-pub struct Player {}
+pub struct Player {
+    sink: Sink,
+    stream_handle: OutputStream,
+}
+
+impl Player {
+    pub fn new() -> Result<Self> {
+        let stream_handle = OutputStreamBuilder::open_default_stream()?;
+        Ok(Player {
+            sink: Sink::connect_new(stream_handle.mixer()),
+            stream_handle: stream_handle,
+        })
+    }
+
+    pub fn play(&self) {
+        self.sink.play();
+    }
+
+    pub fn pause(&self) {
+        self.sink.pause();
+    }
+
+    pub fn volume(&self) -> Volume {
+        self.sink.volume() as Volume
+    }
+
+    pub fn playback_rate(&self) -> PlaybackRate {
+        self.sink.speed() as PlaybackRate
+    }
+
+    pub fn playback_status(&self) -> PlaybackStatus {
+        if self.sink.empty() {
+            PlaybackStatus::Stopped
+        } else if self.sink.is_paused() {
+            PlaybackStatus::Paused
+        } else {
+            PlaybackStatus::Playing
+        }
+    }
+}
---
+++ firefly-core/src/player/tests.rs
@@ -0,0 +1,14 @@
+use mpris_server::PlaybackStatus;
+
+use crate::player::Player;
+
+#[test]
+fn new() {
+    assert!(Player::new().is_ok())
+}
+
+#[test]
+fn play() {
+    let player = Player::new().unwrap();
+    assert_eq!(player.playback_status(), PlaybackStatus::Paused)
+}
