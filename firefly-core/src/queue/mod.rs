4a2c56b86c727ba9f0e962a9521335fa428d6093
lyns0 <disse.sl@proton.me>
2025-12-13 16:54:00 +0700

    feat: impl queue

--- firefly-core/src/queue/mod.rs
+++ firefly-core/src/queue/mod.rs
@@ -2,9 +2,45 @@ use std::collections::VecDeque;
 
 use crate::track::Track;
 
+/// Struct to act as a queue of tracks.
 pub struct Queue {
-    current_track: Track,
+    current_track: Option<Track>,
     previous_tracks: Vec<Track>,
     next_tracks: VecDeque<Track>,
 }
 
+impl Queue {
+    /// Skip to the next track and push current track to the previous-track stack.
+    pub fn next(&mut self) {
+        if !self.next_tracks.is_empty()
+            && let Some(next) = self.next_tracks.pop_front()
+        {
+            if let Some(curr) = self.current_track.take() {
+                self.previous_tracks.push(curr);
+            }
+            self.current_track = Some(next);
+        }
+    }
+
+    /// Go back to the previous track and push-front current track to next-track queue.
+    pub fn prev(&mut self) {
+        if !self.previous_tracks.is_empty()
+            && let Some(prev) = self.previous_tracks.pop()
+        {
+            if let Some(curr) = self.current_track.take() {
+                self.next_tracks.push_front(curr);
+            }
+            self.current_track = Some(prev);
+        }
+    }
+
+    /// Enqueue a new track to the next-track queue.
+    pub fn enqueue(&mut self, track: Track) {
+        self.next_tracks.push_back(track);
+    }
+
+    /// Push-front a new track to the next-track queue.
+    pub fn queue_next(&mut self, track: Track) {
+        self.next_tracks.push_front(track);
+    }
+}
